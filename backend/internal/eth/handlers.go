b015b5efaf20bb12d6f35db5604f2da52b4b8f83
snakoner <andrstroganov@yandex.ru>
2024-12-13 14:52:15 +0300

    add round handler

--- backend/internal/eth/handlers.go
+++ backend/internal/eth/handlers.go
@@ -2,107 +2,81 @@ package eth
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"log"
 	"math/big"
 	"net/http"
 	"strconv"
-	"strings"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/gorilla/mux"
-	"github.com/snakoner/lottery/internal/lottery"
+	"github.com/snakoner/lottery/internal/models"
 )
 
-// type BidResponse struct {
-// 	FromAddress string `json:"fromAddress"`
-// 	TicketNum   int    `json:"ticketNum"`
-// }
+func setHttpError(w http.ResponseWriter, err string, code int) {
+	http.Error(w, err, code)
+}
 
 func (e *EthereumServer) BidHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println("xBidHandler()")
-	// bidResponse := &BidResponse{}
-	// err := json.NewDecoder(r.Body).Decode(bidResponse)
-	// if err != nil {
-	// 	e.logger.Error(err)
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	return
-	// }
 
-	// for len(e.newEvents) == 0 {
-	// }
-
-	// lastEvent := e.newEvents[0]
-
-	// fmt.Println("last event: ", lastEvent)
-
-	// e.newEvents = e.newEvents[:]
-
-	// e.logger.WithFields(logrus.Fields{"BidHandler()": bidResponse})
-	// w.WriteHeader(http.StatusOK)
 }
 
-func (e *EthereumServer) getCurrentBlockNumber() (uint64, error) {
-	blockNumber, err := e.http.cli.BlockNumber(context.Background())
-	if err != nil {
-		return 0, err
-	}
-
-	return blockNumber, err
-}
-
-func (e *EthereumServer) PastRounds(w http.ResponseWriter, r *http.Request) {
+// todo: filter by indexed round
+// RoundHandler is an HTTP handler that processes requests for retrieving bid events
+// corresponding to a specific lottery round.
+//
+// Parameters:
+//   - w: The HTTP response writer used to send responses back to the client.
+//   - r: The HTTP request containing the parameters for the round.
+func (e *EthereumServer) RoundHandler(w http.ResponseWriter, r *http.Request) {
 	roundNum := mux.Vars(r)["number"]
 	round, err := strconv.ParseInt(roundNum, 10, 64)
 	if err != nil {
 		e.logger.Error(err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		setHttpError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	eventSig := []byte("Bid(address,uint256,uint256,uint256)")
-	eventHash := crypto.Keccak256Hash(eventSig)
-	valueToFilter := big.NewInt(round)
-
-	fmt.Println("value to filter: ", valueToFilter)
-
-	query := ethereum.FilterQuery{
-		FromBlock: big.NewInt(0), // Начальный блок
-		ToBlock:   nil,           // Конечный блок (nil для текущего)
-		Addresses: []common.Address{e.contractAddress},
-		Topics:    [][]common.Hash{{eventHash}, {common.BigToHash(valueToFilter)}},
+	opts := &bind.FilterOpts{
+		Start:   uint64(big.NewInt(0).Int64()),
+		End:     nil,
+		Context: context.Background(),
 	}
 
-	logs, err := e.http.cli.FilterLogs(context.Background(), query)
+	logs, err := e.http.inst.FilterBid(opts, nil)
 	if err != nil {
 		e.logger.Error(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		setHttpError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Println(len(logs))
-
-	contractABI, err := abi.JSON(strings.NewReader(`[{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":true,"name":"round","type":"uint256"}],"name":"Bid","type":"event"}]`))
-	for _, vLog := range logs {
-		fmt.Printf("Raw Log: %+v\n", vLog)
-
-		// Распаковка данных события
-		// var result struct {
-		// 	Value  *big.Int
-		// 	Sender common.Address
-		// }
-		result := &lottery.ContractBid{}
-		err := contractABI.UnpackIntoInterface(&result, "Bid", vLog.Data)
-		if err != nil {
-			log.Printf("Failed to unpack log data: %v", err)
+	bidEvents := &models.BidEvents{}
+	for logs.Next() {
+		if logs.Error() != nil {
+			e.logger.Error("Log error: %v", logs.Error())
 			continue
 		}
 
-		// fmt.Printf("Decoded Event: Value=%d, Sender=%s\n", result.Account[0], result.Sender.Hex())
-		fmt.Println(result)
+		event := logs.Event // Автоматически распарсенное событие
+		if round == event.Round.Int64() {
+			bidEvents.Events = append(bidEvents.Events, &models.BidEvent{
+				Account:   event.Account.Hex(),
+				Amount:    event.Amount.Int64(),
+				Timestamp: event.Timestamp.Int64(),
+				Round:     event.Round.Int64(),
+			})
+		}
 	}
 
+	b, err := json.Marshal(bidEvents)
+	if err != nil {
+		e.logger.Error(err)
+		setHttpError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(b))
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
 }
