ad0acbfc191b8a34751c154766897c4fb96e52de
Vsevolod Bursa <bursaseva1@gmail.com>
2024-12-18 15:56:53 +0300

    what switch

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,2 +1,2 @@
-export const ALCHEMY_RPC_URL = 'https://eth-sepolia.g.alchemy.com/v2/QtPw5bLONCtW00agVEhE66pb1Vsv9RnC';
-export const CONTRACT_ADDRESS = '0xE8f0b7144F2be28FE6Af69f15658d7b197Bf9f11';
+export const ALCHEMY_RPC_URL = 'https://eth-holesky.g.alchemy.com/v2/QtPw5bLONCtW00agVEhE66pb1Vsv9RnC';
+export const CONTRACT_ADDRESS = '0x5B4Ac9c70E46e787df176c31d152c582Af27be76';
