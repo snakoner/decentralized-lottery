e1218bd7f7e7459538fdd20f4e38294bc9b8561e
snakoner <andrstroganov@yandex.ru>
2024-12-29 16:04:17 +0300

    update

---
+++ frontend_v2/src/components/constants.tsx
@@ -0,0 +1,37 @@
+import { network } from "hardhat";
+
+export const ALCHEMY_RPC_URL = 'https://eth-sepolia.g.alchemy.com/v2/UtjFRzFoEQd533NSskUCCCKEpW7z93t2';
+export const CONTRACT_ADDRESS = '0x87Ec2437F2A3451385dbC31FdcfE40a1B1814E7F';
+export const CONTRACT_ABI = [
+    "event Bid(address indexed account, uint amount, uint timestamp, uint indexed round)",
+    "event WinnerSelected(address indexed account, uint amount, uint indexed round)",
+    "event Withdraw(address indexed account, uint amount)",
+    "event Deposit(address indexed user, uint amount, uint timestamp)",
+    "function balances(address) public view returns (uint256)",
+    "function weights(uint256, address) public view returns (uint256)",
+    "function totalWeight(uint256) public view returns (uint256)",
+    "function participants(uint256) public view returns (address)",
+    "function bid(uint amount) external payable",
+    "function withdraw(uint amount) external",
+    "function getParticipantsNumber() external view returns (uint)",
+    "function getTimeLeft() external view returns (uint)",
+    "function bidFromBalance(uint amount) external",
+    "function ticketPrice() public view returns (uint256)",
+    "function round() public view returns (uint256)",
+    "function deposit() external payable"
+];
+
+export const localStorageWalletConnectHandler = () => {
+    if (localStorage.getItem('walletConnected') === undefined) {
+        localStorage.setItem('walletConnected', 'false');
+    }
+
+    return localStorage.getItem('walletConnected') === 'true' ? true : false;
+}
+
+export const supportedChains = [
+    {
+        network: "sepolia",
+        chainId: 11155111,
+    },
+];
--- frontend_v2/src/components/layout/Header.tsx
+++ frontend_v2/src/components/layout/Header.tsx
@@ -2,23 +2,140 @@ import { Button } from "@/components/ui/button";
 import { Wallet } from "lucide-react";
 import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { Logo } from "@/components/ui/logo";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { ALCHEMY_RPC_URL, supportedChains, localStorageWalletConnectHandler } from "../constants.tsx";
+import { add } from "date-fns";
+
+const convertSupportedChains = (): Map<bigint, ethers.Network> => {
+    let map = new Map<bigint, ethers.Network>();
+
+    for (const suppChains of supportedChains) {
+        map.set(
+            ethers.toBigInt(suppChains.chainId), 
+            new ethers.Network(suppChains.network, suppChains.chainId)
+        );
+    }
+
+    return map;
+}
 
 export default function Header() {
-  return (
-    <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
-        <Logo />
-        <nav className="flex items-center space-x-4">
-          <ThemeToggle />
-          <Button
-            variant="outline"
-            className="gap-2 hover:scale-105 transition-all hover:bg-purple-50 hover:border-purple-200 hover:text-purple-600 dark:hover:bg-purple-900 dark:hover:border-purple-700 dark:hover:text-purple-400"
-          >
-            <Wallet className="h-4 w-4" />
-            Connect Wallet
-          </Button>
-        </nav>
-      </div>
-    </header>
-  );
+	const [walletError, setWalletError] = useState<string|null>(null);
+	const [account, setAccount] = useState<string|null>(null);
+	const [network, setNetwork] = useState<ethers.Network|null>(null);
+    const [walletBalance, setWalletBalance] = useState<string|null>(null);
+    const [connected, setConnected] = useState<boolean>(false);
+
+
+	const walletBalanceFormat = (balance: bigint) => {
+        return ethers.formatUnits(balance).slice(0, 6);        
+    }
+	
+	const connectWallet = async () => {
+		if (typeof window.ethereum === undefined) {
+			setWalletError("MetaMask is not installed. Please install it to use this feature.");
+			console.log(walletError);
+			return;
+		}
+
+		try {
+			const accounts = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+
+			if (accounts.length === 0) {
+				localStorage.setItem('walletConnected', 'false');
+				return;
+			}
+
+			setAccount(accounts[0]);
+			setConnected(true);
+			walletAddressSet(accounts[0]);
+			localStorage.setItem('walletConnected', 'true');
+			const walletConnectButton = document.getElementById('wallet-connect-button');
+			if (walletConnectButton) {
+				walletConnectButton.onclick = disconnectWallet;
+			}
+
+			const remoteProvider = new ethers.JsonRpcProvider(ALCHEMY_RPC_URL);
+			const provider = new ethers.BrowserProvider(window.ethereum);
+
+			const chainID =  (await provider.getNetwork()).chainId;
+			if (convertSupportedChains().get(chainID) === undefined) {
+				await window.ethereum.request({
+					method: 'wallet_switchEthereumChain',
+					params: [{ chainId: '0x' + supportedChains[0].chainId.toString(16) }], // chainId в формате hex (например, '0x1' для Ethereum Mainnet)
+				});
+			}
+
+			setNetwork((await provider.getNetwork()));
+			setWalletBalance(walletBalanceFormat(await remoteProvider.getBalance(accounts[0])));
+		} catch (err) {
+			setWalletError("Failed to connect wallet. Please try again.");
+			console.log(walletError);
+		}
+	}
+
+	const connectButtonSet = (data: string) => {
+		const element = document.getElementById('wallet-connect-button');
+		if (element !== undefined) {
+			element.innerHTML = data;
+		}
+	}
+
+	const walletAddressSet = (acc: string) => {
+		const slicedWalletAddress = () => {
+			if (!account) {
+				return "";
+			}
+	
+			return acc.slice(0, 6) + "..." + acc.slice(-4);
+		};
+
+		connectButtonSet(slicedWalletAddress());
+	}
+
+	const disconnectWallet = () => {
+		console.log("disconnectWallet");
+        setConnected(false);
+        localStorage.setItem('walletConnected', 'false');
+        setAccount(null);
+        setWalletBalance(null);
+        setNetwork(null);
+		connectButtonSet("Connect wallet");
+		const walletConnectButton = document.getElementById('wallet-connect-button');
+		if (walletConnectButton) {
+			walletConnectButton.onclick = connectWallet;
+		}
+
+    }
+
+	useEffect(() => {
+		console.log('use effect');
+        const walletConnected = localStorageWalletConnectHandler();
+		if (walletConnected) {
+            connectWallet();
+        }
+    }, [account]);
+
+	return (
+		<header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+		<div className="container flex h-16 items-center justify-between">
+			<Logo />
+			<nav className="flex items-center space-x-4">
+			<ThemeToggle />
+			<Button
+				onClick={connectWallet}
+				id="wallet-connect-button"
+				variant="outline"
+				className="gap-2 hover:scale-105 transition-all hover:bg-purple-50 hover:border-purple-200 hover:text-purple-600 dark:hover:bg-purple-900 dark:hover:border-purple-700 dark:hover:text-purple-400"
+			>
+				<Wallet className="h-4 w-4" />
+				Connect Wallet
+			</Button>
+			</nav>
+		</div>
+		</header>
+	);
 }
